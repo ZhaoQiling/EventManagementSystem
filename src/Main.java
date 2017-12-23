d087db403a8e43adad5ed01ecbd8d16417705a82
HAPPY306 <HAPPY306@DESKTOP-8176B9B>
2017-12-23 21:19:46 +0800

    fucking jdbc  fucking port

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
-
+import java.sql.*;
+import com.microsoft.sqlserver.jdbc.*;  
 public class Main {
-
+	public static final String connectionString = 
+//			"server=.\\SQLEXPRESS;uid=sa;pwd=sa;datebase=EMDB;";
+			"jdbc:sqlserver://localhost:1433;"  
+			+ "database=EMDB;"  
+			+ "user=sa;"  
+			+ "password=sa;";  
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+        // Declare the JDBC objects.  
+        Connection connection = null;  
+        Statement statement = null;   
+        ResultSet resultSet = null;  
+        PreparedStatement prepsInsertProduct = null;  
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
+			connection = DriverManager.getConnection(connectionString);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
